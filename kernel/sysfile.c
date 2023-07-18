a70417016a4b531d60e0a3cfc72e14a89be74fd1
zbtrs <1339623011@qq.com>
2023-07-18 11:20:34 +0800

    修复了getcwd的问题

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -562,10 +562,13 @@ sys_getcwd(void)
     }
   }
 
-  // if (copyout(myproc()->pagetable, addr, s, strlen(s) + 1) < 0)
+  if (copyout(myproc()->pagetable, addr, s, strlen(s) + 1) < 0)
+    return -1;
+  /*
   if (copyout2(addr, s, strlen(s) + 1) < 0)
     return -1;
-  
+  */
+
   return addr;
 
 }
