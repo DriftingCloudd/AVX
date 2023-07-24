6fcc9b18e4e9cf83639473b3c854a5e93bdf87fc
asterich <asterich123@gmail.com>
2023-07-24 20:12:34 +0800

    added: files for socket

---
+++ kernel/syssocket.c
@@ -0,0 +1,121 @@
+#include "include/sysnum.h"
+#include "include/types.h"
+#include "include/riscv.h"
+#include "include/param.h"
+#include "include/stat.h"
+#include "include/spinlock.h"
+#include "include/proc.h"
+#include "include/sleeplock.h"
+#include "include/file.h"
+#include "include/pipe.h"
+#include "include/fcntl.h"
+#include "include/socket.h"
+#include "include/syscall.h"
+#include "include/string.h"
+#include "include/printf.h"
+#include "include/vm.h"
+#include "include/mmap.h"
+#include "include/sysinfo.h"
+
+uint64
+sys_socket(void) {
+    int domain, type, protocol;
+    if (argint(0, &domain) < 0 || argint(1, &type) < 0 || argint(2, &protocol) < 0) {
+        return -1;
+    }
+    return do_socket(domain, type, protocol);
+}
+
+uint64
+sys_bind(void) {
+    int sockfd;
+    struct sockaddr *addr;
+    socklen_t addrlen;
+    if (argint(0, &sockfd) < 0 || argaddr(1, (void *)&addr) < 0 || argint(2, &addrlen) < 0) {
+        return -1;
+    }
+    return do_bind(sockfd, addr, addrlen);
+}
+
+uint64
+sys_listen(void) {
+    int sockfd, backlog;
+    if (argint(0, &sockfd) < 0 || argint(1, &backlog) < 0) {
+        return -1;
+    }
+    return do_listen(sockfd, backlog);
+}
+
+uint64
+sys_connect(void) {
+    int sockfd;
+    struct sockaddr *addr;
+    socklen_t addrlen;
+    if (argint(0, &sockfd) < 0 || argaddr(1, (void *)&addr) < 0 || argint(2, &addrlen) < 0) {
+        return -1;
+    }
+    return do_connect(sockfd, addr, addrlen);
+}
+
+uint64
+sys_accept(void) {
+    int sockfd;
+    struct sockaddr *addr;
+    socklen_t *addrlen;
+    if (argint(0, &sockfd) < 0 || argaddr(1, (void *)&addr) < 0 || argint(2, (int *)addrlen) < 0) {
+        return -1;
+    }
+    return do_accept(sockfd, addr, addrlen);
+}
+
+uint64
+sys_sendto(void) {
+    int sockfd;
+    void *buf;
+    size_t len;
+    int flags;
+    struct sockaddr *dest_addr;
+    socklen_t addrlen;
+    if (argint(0, &sockfd) < 0 || argaddr(1, (void *)&buf) < 0 || argint(2, &len) < 0 || argint(3, &flags) < 0 || argaddr(4, (void *)&dest_addr) < 0 || argint(5, &addrlen) < 0) {
+        return -1;
+    }
+    return do_sendto(sockfd, buf, len, flags, dest_addr, addrlen);
+}
+
+uint64
+sys_recvfrom(void) {
+    int sockfd;
+    void *buf;
+    size_t len;
+    int flags;
+    struct sockaddr *src_addr;
+    socklen_t *addrlen;
+    if (argint(0, &sockfd) < 0 || argaddr(1, (void *)&buf) < 0 || argint(2, &len) < 0 || argint(3, &flags) < 0 || argaddr(4, (void *)&src_addr) < 0 || argint(5, (int *)addrlen) < 0) {
+        return -1;
+    }
+    return do_recvfrom(sockfd, buf, len, flags, src_addr, addrlen);
+}
+
+uint64
+sys_getsockname(void) {
+    int sockfd;
+    struct sockaddr *addr;
+    socklen_t *addrlen;
+    if (argint(0, &sockfd) < 0 || argaddr(1, (void *)&addr) < 0 || argint(2, (int *)addrlen) < 0) {
+        return -1;
+    }
+    return do_getsockname(sockfd, addr, addrlen);
+}
+
+uint64
+sys_setsockopt(void) {
+    int sockfd;
+    int level;
+    int optname;
+    void *optval;
+    socklen_t optlen;
+    if (argint(0, &sockfd) < 0 || argint(1, &level) < 0 || argint(2, &optname) < 0 || argaddr(3, (void *)&optval) < 0 || argint(4, &optlen) < 0) {
+        return -1;
+    }
+    return do_setsockopt(sockfd, level, optname, optval, optlen);
+}
