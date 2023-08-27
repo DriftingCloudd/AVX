4263cd02285bfac394d7060665517aa9eeef2406
zxt <1163862613@qq.com>
2023-08-27 13:49:43 +0800

    change

--- cp_net.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo mount fs.img /mnt
-sudo cp /home/zbtrs/testsuits-for-oskernel/sdcard/netperf /mnt/
-sudo cp /home/zbtrs/testsuits-for-oskernel/sdcard/netserver /mnt/
-sudo umount fs.img
